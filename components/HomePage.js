dabddf8cdc2a80a895266cbbc912df37780b0a42
Kalpan Chatterjee <chatterjeekalpan@gmail.com>
2021-07-08 21:04:42 +0530

    cmmited

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { ShoppingBagIcon } from "@heroicons/react/outline";
 
 const HomePage = ({ data, count, setCount }) => {
   return (
@@ -9,24 +10,24 @@ const HomePage = ({ data, count, setCount }) => {
     >
       {data.map((value, index) => {
         return (
-          <div key={index} className="tracking-widest text-black-300 p-3">
+          <div
+            key={index}
+            className="tracking-widest text-black-300 p-3 border-blue-100"
+          >
             <div className="mx-5">
               <Image src={value.image} height={150} width={150}></Image>
             </div>
             <p className="truncate max-w-md">{value.title}</p>
             <p className="truncate max-w-md">{value.description}</p>
             <div className="flex min-w-full justify-between pt-2 cursor-pointer">
-              <p>{value.price}</p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 items-end mt-1"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                title="Add to Cart"
+              <p>$ {value.price}</p>
+              <ShoppingBagIcon
+                className=""
                 onClick={() => setCount(count + 1)}
-              >
-                <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-              </svg>
+                height={25}
+                width={25}
+                color="yellow"
+              />
             </div>
           </div>
         );
